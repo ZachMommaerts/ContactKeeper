6c0bb14d2466debc5eb58a3fc74080bd8f43d79f
ZachMommaerts <zmommaerts@gmail.com>
2023-11-29 21:36:14 -0600

    Initial Commit

---
+++ ContactKeeper/ContactKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactKeeperApp.swift
+//  ContactKeeper
+//
+//  Created by Zach Mommaerts on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
